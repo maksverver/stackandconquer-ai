a47855d894347e04c77775686ecf95236e1c3228
Maks Verver <maks@verver.ch>
2024-10-23 22:11:47 +0200

    Beef up ESLint config.

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,36 @@
+import importPlugin from 'eslint-plugin-import';
 import js from "@eslint/js";
+
+const rules = {
+  ...js.configs.recommended.rules,
+  ...importPlugin.flatConfigs.recommended.rules,
+};
+
+const plugins = {
+  ...js.configs.recommended.plugins,
+  ...importPlugin.flatConfigs.recommended.plugins,
+};
 
 export default [
   {
-    ...js.configs.recommended,
+    rules,
+    plugins,
     files: ['src/**/*.js'],
     languageOptions: {
       ecmaVersion: 6,
+      sourceType: 'module',
       globals: {
         'game': 'readonly',
       }
+    }
+  },
+  {
+    rules,
+    plugins,
+    files: ['test/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     }
   }
 ];
